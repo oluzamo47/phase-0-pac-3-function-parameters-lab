c92c4839d9fb01d7f1ace8562ea8d4764eedf34c
Replit user <>
2023-03-09 14:29:38 +0000

    Initial commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,14 +1,22 @@
 require ( './root.js' );
 
+function introduction(name)  {
+  return(`Hi, my name is ${name}.`);
+}
+introduction('Aki');
 
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
     expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
     expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
+
   })
 })
-
-describe('introductionWithLanguage(name, language)', function() {
+function introductionWithLanguageOptional(name, language) {
+  console.log(`"Hi, my name is ${name} and I am learning to program in ${language}."`)
+introductionWithLanguageOptional('name','language'); 
+  
+describe('introductionWithLanguage(name,language)', function() {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
     expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
     expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
@@ -25,4 +33,5 @@ describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
     expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
   })
-})
+});
+}
